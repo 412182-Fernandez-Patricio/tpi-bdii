b7b82a5c5d84a4971fdad61d04e9b3ba64ae4451
412182-FERNANDEZ <412182@tecnicatura.frc.utn.edu.ar>
2025-06-14 17:02:53 -0300

    (Otra vez me paso lo mismo)Agregue las otras pag
    Hay que cambiiar todo el back, para q funque con los datos reales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {TopPeliculasComponent} from './components/top-peliculas/top-peliculas';
+import {ReportePorDirectorComponent} from './components/reporte-por-director/reporte-por-director';
+import {ReportePorActor} from './components/reporte-por-actor/reporte-por-actor';
+import {ReportePorCalificacionComponent} from './components/reporte-por-calificacion/reporte-por-calificacion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'top-peliculas', pathMatch: "full"},
+  {path: 'top-peliculas', component: TopPeliculasComponent},
+  {path: 'reporte-por-director', component: ReportePorDirectorComponent},
+  {path: 'reporte-por-actor', component: ReportePorActor},
+  {path: 'reporte-por-calificacion', component: ReportePorCalificacionComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import {Navbar} from './components/navbar/navbar';
-import {MovieCardComponent} from './components/movie-card/movie-card';
-import {TopPeliculasComponent} from './components/top-peliculas/top-peliculas';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, MovieCardComponent, TopPeliculasComponent],
+  imports: [RouterModule, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected title = 'tpi-bdii';
 }
+
