d556a417afe9b809a66f4f8a7b25f3460ae4a14f
407926-ATAMPIZ <407926@tecnicatura.frc.utn.edu.ar>
2025-06-18 23:27:51 -0300

    Carga de tarjetas de películas en base a las obtenidas con la API y cambio de color a la estrellita je

--- src/app/components/top-peliculas/top-peliculas.ts
+++ src/app/components/top-peliculas/top-peliculas.ts
@@ -1,14 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MovieCardComponent } from '../movie-card/movie-card';
+import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MovieCardComponent} from '../movie-card/movie-card';
 
 interface Pelicula {
-  title: string;
-  posterUrl: string;
-  year: number;
-  duration: string;
-  rating: number;
+  "titulo": string,
+  "añoLanzamiento": number,
+  "director": string,
+  "rating": number,
+  "categorias": string[],
+  "duracion": number,
+  "poster": string
 }
 
 @Component({
@@ -17,13 +19,16 @@ interface Pelicula {
   imports: [CommonModule, FormsModule, MovieCardComponent],
   templateUrl: './top-peliculas.html',
 })
+
 export class TopPeliculasComponent {
+
+  endpoint : string = "http://localhost:8080/api/v1/views/peliculasConPoster";
   // Filtros
-  desde = '';
-  hasta = '';
-  topX = 5;
-  genero = '';
-  director = '';
+  // desde = '';
+  // hasta = '';
+  // topX = 5;
+  // genero = '';
+  // director = '';
 
   // Datos fake de prueba
   peliculas = signal<Pelicula[]>([]);
@@ -32,59 +37,62 @@ export class TopPeliculasComponent {
   generos = ['Acción', 'Comedia', 'Drama', 'Terror'];
   directores = ['Nolan', 'Spielberg', 'Scorsese'];
 
-  buscar() {
+  async buscar() {
     // Esto simula la llamada al back
-    this.peliculas.set([
-      {
-        title: 'Inception',
-        posterUrl: 'https://http2.mlstatic.com/D_NQ_NP_860530-MLA81194764967_122024-O.webp',
-        year: 2010,
-        duration: '2h 28m',
-        rating: 9.2,
-      },
-      {
-        title: 'Interstellar',
-        posterUrl: 'https://mythicwall.com/cdn/shop/files/Interstellar_2BMovie_2B_2Bposter_2BPrint_2BWall_2BArt_2BPoster_2B1-W0pfS_1024x1024.jpg?v=1712634286',
-        year: 2014,
-        duration: '2h 49m',
-        rating: 8.6,
-      },
-      {
-        title: 'The Dark Knight',
-        posterUrl: 'https://m.media-amazon.com/images/I/81rGCm0PyHL.jpg',
-        year: 2008,
-        duration: '2h 32m',
-        rating: 9.0,
-      },
-      {
-        title: 'The Matrix',
-        posterUrl: 'https://www.originalfilmart.com/cdn/shop/products/the_matrix_1999_advance_original_film_art_d47e19ed-a6b9-47d3-985c-30197dd9ccc5_5000x.jpg?v=1599757481',
-        year: 1999,
-        duration: '2h 16m',
-        rating: 8.7,
-      },
-      {
-        title: 'Pulp Fiction',
-        posterUrl: 'https://i5.walmartimages.com/asr/ce193b8f-4979-4dd4-89fb-cb5bda97b87b.47bd5508be83004cfd0b4ae1123a208e.jpeg',
-        year: 1994,
-        duration: '2h 34m',
-        rating: 8.9,
-      },
-      {
-        title: 'The Shawshank Redemption',
-        posterUrl: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
-        year: 1994,
-        duration: '2h 22m',
-        rating: 9.3,
-      },
-      {
-        title: 'Fight Club',
-        posterUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg',
-        year: 1999,
-        duration: '2h 19m',
-        rating: 8.8,
-      },
-    ]);
+    const peliculas = await fetch(this.endpoint).then(response => response.json());
+    console.log(peliculas);
+    this.peliculas.set(peliculas);
+    // this.peliculas.set([
+    //   {
+    //     title: 'Inception',
+    //     posterUrl: 'https://http2.mlstatic.com/D_NQ_NP_860530-MLA81194764967_122024-O.webp',
+    //     year: 2010,
+    //     duration: '2h 28m',
+    //     rating: 9.2,
+    //   },
+    //   {
+    //     title: 'Interstellar',
+    //     posterUrl: 'https://mythicwall.com/cdn/shop/files/Interstellar_2BMovie_2B_2Bposter_2BPrint_2BWall_2BArt_2BPoster_2B1-W0pfS_1024x1024.jpg?v=1712634286',
+    //     year: 2014,
+    //     duration: '2h 49m',
+    //     rating: 8.6,
+    //   },
+    //   {
+    //     title: 'The Dark Knight',
+    //     posterUrl: 'https://m.media-amazon.com/images/I/81rGCm0PyHL.jpg',
+    //     year: 2008,
+    //     duration: '2h 32m',
+    //     rating: 9.0,
+    //   },
+    //   {
+    //     title: 'The Matrix',
+    //     posterUrl: 'https://www.originalfilmart.com/cdn/shop/products/the_matrix_1999_advance_original_film_art_d47e19ed-a6b9-47d3-985c-30197dd9ccc5_5000x.jpg?v=1599757481',
+    //     year: 1999,
+    //     duration: '2h 16m',
+    //     rating: 8.7,
+    //   },
+    //   {
+    //     title: 'Pulp Fiction',
+    //     posterUrl: 'https://i5.walmartimages.com/asr/ce193b8f-4979-4dd4-89fb-cb5bda97b87b.47bd5508be83004cfd0b4ae1123a208e.jpeg',
+    //     year: 1994,
+    //     duration: '2h 34m',
+    //     rating: 8.9,
+    //   },
+    //   {
+    //     title: 'The Shawshank Redemption',
+    //     posterUrl: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
+    //     year: 1994,
+    //     duration: '2h 22m',
+    //     rating: 9.3,
+    //   },
+    //   {
+    //     title: 'Fight Club',
+    //     posterUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg',
+    //     year: 1999,
+    //     duration: '2h 19m',
+    //     rating: 8.8,
+    //   },
+    // ]);
 
   }
 }
